4e93c9bc08929302435737b93dc2481c9a1bf9fb
Fallog <fallog@mailo.com>
2022-02-21 17:51:32 +0100

    How to uptade + test mocking Yanis

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is the main file of our 'Ecosysteme' project 
+# Here is the list of commands needed to save the changes and update the files to Github.com
+# git add .
+# git commit -m "Ecrire un message résumant les changemets"
+# git push  
+
+# Testing updating this
+yanis = "Worst guy in the world"
+sindt_baret = "Pas ouf comme nom"
+
