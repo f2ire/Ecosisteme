4a59b50775ab9d503936f24db4d1133c66678746
Fallog <fallog@mailo.com>
2022-03-18 17:28:40 +0100

    Creating the Cell class. Adding moving method that allows the cell to move randomly on the environment

--- main.py
+++ main.py
@@ -8,32 +8,40 @@
 # MODULES #
 ###########
 import pygame
+import time
+import cell
 
 ###########
 # MAIN CODE #
 ###########
+
+# INITIALISATION
 pygame.init() # Initiation of pygame -> mandatory
+
+# CREATION OF THE WINDOW
 window_edge = 600 # in pixels -> wanted edge size for the display window
 main_window = pygame.display.set_mode((window_edge,window_edge)) # Creation of the main window -> size : window_surface x window_surface
-    
-small_rect = (300, 200, 150, 90) # Datas on size and position of a rectangle  
-some_color = (255, 0, 0) # RED for the rectangle
-bg_color = (0,200,255) # SKY BLUE for the bg color of our window
-    
-    
-    # Game loop
+bg_color = (255,255,255) # WHITE for the background color
+main_window.fill(bg_color) # Colouring the window
+
+# ADDING A CELL TO THE ENVIRONMENT
+first_cell = cell.Cell(window_edge//2,window_edge//2) # Creating a cell in the middle of our window
+main_window.fill(first_cell.color,first_cell.attributes)
+
+# GAME LOOP
 while True:
-    event = pygame.event.poll()
- # Collecting an event from the user 
+    event = pygame.event.poll() # Collecting an event from the user 
             
     # The loop (and the code) terminates if the user click on the close button of the window
     if event.type == pygame.QUIT:
         break
-        
-    main_window.fill(bg_color) # Colouring the window
-    main_window.fill(some_color,small_rect) # Colouring a rectangle on the window 
-    pygame.display.flip() # Displaying the window
-    #pygame.quit() # Closing the window if leaving the loop
+    main_window.fill(bg_color) # Resetting the window blank
+    first_cell.moving() # Moving the cell to the right (for now)
+    main_window.fill(first_cell.color,first_cell.attributes)
+    
+    pygame.display.flip() # Displaying the window continuously
+    
+pygame.quit() # Closing the window if leaving the loop
     
 
 
