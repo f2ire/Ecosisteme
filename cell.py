e988458e6ba59608104d8ff4fab2e124f629901c
Fallog <fallog@mailo.com>
2022-03-20 12:34:42 +0100

    Debugging -> code is working

--- cell.py
+++ cell.py
@@ -9,10 +9,9 @@ class Cell:
     """
     The cell object, the fondamental unit of life
     Cells should be capable of moving towards energy&food sources and replicate itself
-    A cell is represented as a small blue square in the environment
+    A cell is represented as a small square in the environment
     """
-    # The dimensions of the cell for the display in the pygame window ->
-    # a small blue square of 10x10 pixels
+    # The dimensions of the cell for the display in the pygame window
     (length, width) = (10, 10)
     birth_color = (255, 102, 0)
     death_color = (0, 12, 255)
@@ -23,8 +22,8 @@ class Cell:
     # The number of times the cell replicates itself in one iteration of the game loop
     growth_rate = 1/4000
 
-    # The oldest age a cell can be. This number is chosen randomly between 5000 and 10000 to simulates diversity
-    max_age = random.randint(5000,10000)
+    # The oldest age a cell can be. This number is chosen randomly between 6000 and 10000 to simulates diversity
+    max_age = random.randint(8000,14000)
 
 
     def __init__(self, pos_x=0, pos_y=0):
@@ -33,7 +32,7 @@ class Cell:
         self.y = pos_y
 
         # Attributes is in this rectangle tuple format to fit to the pygame.fill() method which fills rectangle objects
-        self.attributes = (self.x, self.y, self.length, self.width)
+        self.attributes = (self.x, self.y, self.width, self.length)
 
         # When a cell is created, it age is set on 0. The cell is aging over time and it color is changing with it age
         self.age = 0
@@ -49,12 +48,19 @@ class Cell:
         self.y += self.mvmt_speed * random_direction[1]
         self.attributes = (self.x, self.y, self.length, self.width)
 
+    def isReplicating(self):
+        """
+        Returns True if the cell replicates itself based on its growth_rate
+        """
+        # Boolean telling if the cell is gonna replicates itself
+        is_replicating = random.random() <= self.growth_rate
+        return is_replicating
+
 
     def replication(self):
         """
         The cell makes a copy of itself in one direction accessible around it, this method returns the daughter cell.
-        The number of copy realised is defined by the growth rate.
-        """
+        """        
         # The daughter cell is made in a random direction around mother cell
         random_direction = dir.Direction.get_random_direction()
         new_x = self.x + 10 * random_direction[0]
@@ -62,9 +68,19 @@ class Cell:
         return Cell(new_x, new_y)
 
 
+    def isTooOld(self):
+        """
+        Returns True if the cell's age is superior to its max_age
+        """
+        if self.age > self.max_age:
+            return True
+        else:
+            return False
+
+
     def adapt_color(self):
         """
-        Linear interpolation to determine the cell's color depends of age
+        Linear interpolation to determine the cell's color depending of its age
         """
         alpha = self.age/self.max_age
         self.color = (
@@ -78,7 +94,14 @@ class Cell:
 # MAIN CODE #
 #############
 if __name__ == '__main__':
+    cell_list = []
     cell1 = Cell(50, 50,)
-    for i in range(50):
-        cell1.moving()
-        print(cell1.attributes)
+    cell_list.append(cell1)
+    i = 0
+    while i < 16000:
+        if cell1.replication() != None:
+            cell_list.append(cell1.replication())
+        else:
+            pass
+        i += 1
+    print(len(cell_list))
--- main.py
+++ main.py
@@ -16,29 +16,6 @@ import data_logger
 #############
 # FUNCTIONS #
 #############
-def isReplicating(cell):
-  """
-  Returns True if the cell is gonna replicates itselfs.
-  """
-  # Random number between 0 and 1 -> if < growth_rate then the cell
-  # replicates itself
-  replication_proba = random.random()
-
-  if replication_proba <= cell.growth_rate:
-    return True
-    
-  else:
-    return False
-
-
-def isTooOld(cell):
-  """
-  Returns True if the cell's age is equal to it max_age
-  """
-  if cell.age > cell.max_age:
-    return True
-  else:
-    return False
 
 
 ###########
@@ -61,6 +38,7 @@ first_cell = cell.Cell(window_edge//2, window_edge//2)
 cells_list = [first_cell]
 main_window.fill(first_cell.color, first_cell.attributes)
 
+# For displaying the number of cells over time
 logger = data_logger.DataLogger(cells_list)
 
 # GAME LOOP
@@ -68,42 +46,43 @@ i = 0
 while True:
   event = pygame.event.poll()  # Collecting an event from the user
 
-  # The loop (and the code) terminates if the user click on the close button
-  # of the window
+  # The loop (and the code) terminates if the user click on the close button of the window
   if event.type == pygame.QUIT:
     break
 
   main_window.fill(bg_color)  # Resetting the window blank
-
-  for cells in cells_list:
+  
+  # If the list is empty we stop the loop
+  if len(cells_list) == 0:
+    break
+  else:
+    for cells in cells_list:
     # Determine if cells shoudl die from age
-    if isTooOld(cells): # Yes -> removes the cell from the list and the loop goes directly on the next cell
-      cells_list.remove(cells)
-      break
-    else: # No -> cells is aging, moving and maybe replicating
-      # cells is aging
-      cells.age += 1
+        if cells.isTooOld(): # Yes -> removes the cell from the list and the loop goes directly on the next cell
+            cells_list.remove(cells)
+            break
+        else: # No -> cells is aging, moving and maybe replicating
+            # cells is aging
+            cells.age += 1
 
-      # cells is moving
-      cells.moving() 
+            # cells is moving
+            cells.moving() 
 
-      # Determine if cells will be replicating it self
-      if isReplicating(cells):
-        daugther_cell = cells.replication()
-        cells_list.append(daugther_cell)  # Adding the daugther cell to the list
-      else:
-        pass
-    
-      
-    main_window.fill(cells.color, cells.attributes)
+            # cells might replicates itself
+            if cells.isReplicating():
+                cells_list.append(cells.replication())
+            else:
+                pass
 
-    cells.adapt_color()
+            # cells is changing color as a function of it age
+            cells.adapt_color()
 
-    
+        main_window.fill(cells.color, cells.attributes)
 
     
   if i % 100 == 0:
     logger.counting_cell()
+
   pygame.display.flip()  # Displaying the window continuously
 
 logger.draw_cell_number_by_time()
