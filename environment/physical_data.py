a21644d98e3863242e1d0dcbae9a608ca0d88d1e
Fallog <fallog@mailo.com>
2022-07-01 12:27:56 +0200

    Ajout de la capacité thermique massique de l'eau

--- environment/physical_data.py
+++ environment/physical_data.py
@@ -6,7 +6,7 @@ import math
 ######################
 # PHYSICAL CONSTANTS #
 ######################
-TIME_ITERATION = 10**(-3) # Number of seconds flowing after each iteration of the game loop 
+TIME_ITERATION: int = 10**(-3) # Number of seconds flowing after each iteration of the game loop 
 # Universal constants
 NA: float = 6.02214076*10**23 # Avogadero constant -> mol-1
 R: float = 8.31446261815324 # Gas constant -> J/K/mol
@@ -14,14 +14,14 @@ KB: float = 1.380649*10**(-23) # Boltzmann constant -> J/mol
 
 
 # Glucose constants
-
 GLUCOSE_ACTIVATION_ENERGY: float = 1.775*10**4 # Activation energy of the glucose-water diffusion -> J/mol
 GLUCOSE_FREQUENCY_FACTOR: float = 8.380046902993091*10**2 # approximation, assuming it is constant and not dependant on temperature -> m2/s
 GLUCOSE_DENSITY: float = 1.54*10**3 # kg/m³
 
 # Water constants
 WATER_DENSITY: float = 1.0*10**3 # kg/m³
-WATER_THERMAL_CONDUCTIVITY = 0.6 # W/m/K
+WATER_THERMAL_CONDUCTIVITY: float = 0.6 # W/m/K
+WATER_HEAT_CAPACITY: int = 4185 # J/K/kg 
 
 
 ###########
