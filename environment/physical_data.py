06a2d89b38f702128366f74573a44e5a5304ce23
Fallog <fallog@mailo.com>
2022-07-01 12:22:09 +0200

    Changement des noms des différentes constantes pour le mettre en majuscules

--- environment/physical_data.py
+++ environment/physical_data.py
@@ -6,19 +6,22 @@ import math
 ######################
 # PHYSICAL CONSTANTS #
 ######################
+TIME_ITERATION = 10**(-3) # Number of seconds flowing after each iteration of the game loop 
 # Universal constants
 NA: float = 6.02214076*10**23 # Avogadero constant -> mol-1
 R: float = 8.31446261815324 # Gas constant -> J/K/mol
 KB: float = 1.380649*10**(-23) # Boltzmann constant -> J/mol
 
+
 # Glucose constants
-activation_energy_glucose: float = 1.775*10**4 # Activation energy of the glucose-water diffusion -> J/mol
-frequency_factor_glucose: float = 8.380046902993091*10**2 # approximation, assuming it is constant and not dependant on temperature -> m2/s
-density_glucose: float = 1.54*10**3 # kg/m³
+
+GLUCOSE_ACTIVATION_ENERGY: float = 1.775*10**4 # Activation energy of the glucose-water diffusion -> J/mol
+GLUCOSE_FREQUENCY_FACTOR: float = 8.380046902993091*10**2 # approximation, assuming it is constant and not dependant on temperature -> m2/s
+GLUCOSE_DENSITY: float = 1.54*10**3 # kg/m³
 
 # Water constants
-density_water: float = 1.0*10**3 # kg/m³
-thermal_conductivity_water = 0.6 # W/m/K
+WATER_DENSITY: float = 1.0*10**3 # kg/m³
+WATER_THERMAL_CONDUCTIVITY = 0.6 # W/m/K
 
 
 ###########
@@ -33,7 +36,7 @@ def computeGlucoseDiffusionCoefficient(temperature: float) -> float:
   Returns:
     float: D, the coefficient of the diffusion of glucose in water solvant, in m²/s
   """
-  return frequency_factor_glucose*math.exp(-activation_energy_glucose/(temperature*NA*KB))
+  return GLUCOSE_FREQUENCY_FACTOR*math.exp(-GLUCOSE_ACTIVATION_ENERGY/(temperature*NA*KB))
 
 
 #############
@@ -42,5 +45,5 @@ def computeGlucoseDiffusionCoefficient(temperature: float) -> float:
 if __name__ == "__main__":
   Diffusion_coef = 0.651
   Temperature = 298.15
-  A = math.exp(-activation_energy_glucose/(NA*KB*Temperature))
+  A = math.exp(-GLUCOSE_ACTIVATION_ENERGY/(NA*KB*Temperature))
   print(Diffusion_coef/A)
