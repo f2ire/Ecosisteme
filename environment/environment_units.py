279b7d4e8116315f5bd25aa4125b328c93d89c48
Fallog <fallog@mailo.com>
2022-07-01 18:42:28 +0200

    Module environment_units complètement fonctionnel

--- environment/environment_units.py
+++ environment/environment_units.py
@@ -2,21 +2,25 @@
 # MODULES #
 ###########
 import math
+import physical_data as phy
 
 ####################
 # CLASS DEFINITION #
 ####################
 class EnvironmentUnit:
-  """The fondamental unit of the environment represented by a square of shape width x length, a volume of 1mm³ and positional coordinates x and y.
+  """The fondamental unit of the environment represented by a cube of shape width x length x height.
   This class is used to keep tracks of the position of entities in the environment. 
   This class is also used to defined other units that will store essential data for the environment.
-  This class contains attributes shared by all the subclasses. 
+  This class contains attributes shared by all the subclasses.
+  For the calculus, an unit is represented as a cube of water.
 
   Attributes :
     width (int): the size of one side of the unit, in m
     length (int): the size of one side of the unit, in m
+    height (int): the size of one side of the unit, in m
     surface (float): surface of one unit, in m²
     volume (float): volume of one unit, in m³
+    mass (float): total mass of one unit, calculated as it's a cube of water
     x (int): positional x coordinates of the unit
     y (int): positional y coordinates of the unit
     is_occupied (bool): True if the occupation unit is occupated by an entity of the environment, False if nothing lays in it
@@ -26,6 +30,7 @@ class EnvironmentUnit:
   height: int = 2*10**(-3) # m
   surface: float = width * length # m²
   volume: float =  width * length * height # m³
+  mass: float = volume * phy.WATER_DENSITY # kg
 
   x: int
   y: int
@@ -40,11 +45,15 @@ class EnvironmentUnit:
 
 
 class TemperatureUnit(EnvironmentUnit):
-  """An environmental unit used to store and modify the temperature of the environment
+  """An environmental unit used to store and modify the temperature of the environment.
   
   Attributes :
     temperature (float): temperature of the unit, in Kelvin
     color (tuple): tuple in RGB format, used to display a temperature map of the environment
+  
+  Functions : 
+    changeTemperature
+    adaptTemperatureColor
   """
   temperature: float
   color: tuple
@@ -61,17 +70,18 @@ class TemperatureUnit(EnvironmentUnit):
     return super().__str__() + f"Having a temperature of : {self.temperature} K\nIts color in RGB encoding is : ({self.color[0]},{self.color[1]},{self.color[2]})\n"
 
   def changeTemperature(self, thermal_flux: float) -> None:
-    """Update the actual temperature attribute of the temperature unit according to the thermal flux it is receiving
+    """Update the actual temperature attribute of the temperature unit according to the thermal flux it's receiving.
+    Calculus is made following the equation Tf = Ti + Q/(m*c). Tf and Ti are final and initial temperature of the unit.
+    m is the total mass of the unit, c it's heat capacity and Q the received thermal energy
 
     Args:
       thermal_flux (float): thermal flux the unit is receiving, in W
     """
-    self.temperature = thermal_flux # not the right calculus, convert the thermal flux into actual temperature
-    self.adaptTemperatureColor()
+    self.temperature += phy.computeThermalEnergy(thermal_flux) / (self.mass * phy.WATER_HEAT_CAPACITY) 
   
   def adaptTemperatureColor(self) -> None:
     """Modifies the unit's temperature color according to its temperature.
-    The color should be blue when the temperature is low, green when it is optimal and red when it's too high
+    The color should be blue when the temperature is low, green when it is optimal and red when it's too high.
     """
     self.color = (255/2*math.erf((self.temperature-350)/50)+255/2,
                               255*math.exp(-1/1000*(self.temperature-320)**2),
@@ -84,6 +94,10 @@ class GlucoseUnit(EnvironmentUnit):
   Attributes :
     glucose_concentration (float): glucose concetration of the unit, in km/m³
     color (tuple): tuple in RGB format, used to display a glucose map of the environment
+  
+  Functions : 
+    changeGlucoseConcentration
+    adaptGlucoseColor
   """
   glucose_concentration: float
   color: tuple
@@ -133,5 +147,5 @@ if __name__ == "__main__":
   print(test_glucose_unit) # OK
 
   # Variable change tests
-  test_temperature_unit.changeTemperature(thermal_flux=330) ; print(test_temperature_unit.temperature == 330) # OK mais fonction pas correcte
+  test_temperature_unit.changeTemperature(thermal_flux=0.5) ; print(test_temperature_unit.temperature == (5*10**(-4)/(8*10**(-6)*4.185*10**3))+298.15)
   test_glucose_unit.changeGlucoseConcentration(5*10**(-10)) ; print(test_glucose_unit.glucose_concentration == 0.005+0.5/8)
